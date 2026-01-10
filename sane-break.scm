7261e0a9e78954958a43a53084f38da89b9b92ab
Adroit <3915447+Erudition@users.noreply.github.com>
2026-01-10 00:22:32 -0600

    sane-break: Update to 0.9.5 (Qt6 migration).

--- sane-break.scm
+++ sane-break.scm
@@ -1,6 +1,6 @@
 (define-module (sane-break)
   #:use-module (guix packages)
-  #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix build-system qt)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages qt)
@@ -11,31 +11,36 @@
   #:use-module (gnu packages vulkan)
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
   #:use-module (gnu packages kde-frameworks))
 
 (define-public sane-break
   (package
     (name "sane-break")
-    (version "0.8.1")
+    (version "0.9.5")
     (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/AllanChain/sane-break/archive/refs/tags/v" version
-                                  ".tar.gz"))
-              (file-name (string-append name "-" version ".tar.gz"))
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/AllanChain/sane-break")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
               (sha256
                (base32
-                "1h25i2bafrjqjcaw6y9k8pwy8lj5swnwqwvsjffxi6iyqj74190g"))))
+                "1pjxzr7wp7kg8n5vrzngvhp9qb012fpha7sg8lznyhx8an5i8dz1"))))
     (build-system qt-build-system)
+    (arguments (list #:tests? #f))
     (inputs
-     (list qtbase-5
-           qtx11extras
+     (list qtbase
+           qtwayland
+           qtmultimedia
+           wayland
            libxcb
            xcb-util-wm
            libxkbcommon
            kwindowsystem
            vulkan-loader))
     (native-inputs
-     (list pkg-config))
+     (list pkg-config python))
     (home-page "https://github.com/AllanChain/sane-break")
     (synopsis "Take-a-break reminder tool")
     (description "Sane-break is a take-a-break reminder tool that is sane.
