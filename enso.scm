846e5bb0ca808bd23ced444ac8522d2357bf0994
Adroit <3915447+Erudition@users.noreply.github.com>
2026-01-09 22:17:06 -0600

    enso: Refactor as proper module and fix install phase.

--- enso.scm
+++ enso.scm
@@ -1,3 +1,10 @@
+(define-module (enso)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (nonguix build-system binary)
+  #:use-module (guix build-system cargo)
+  #:use-module ((guix licenses) #:prefix license:))
+
 (define-public enso
   (package
     (name "enso")
@@ -18,35 +25,14 @@
                     (lambda _
                       (for-each make-file-writable
                                 '("ya" "yazi"))))
-                  (add-after 'install 'install-completions
+                  (replace 'install
                     (lambda* (#:key outputs #:allow-other-keys)
-                      (let* ((share (string-append (assoc-ref outputs "out")
-                                                   "/share"))
-                             (bash (string-append share
-                                    "/bash-completion/completions"))
-                             (zsh (string-append share
-                                                 "/zsh/vendor_completions")))
-                        (mkdir-p bash)
-                        (mkdir-p zsh)
-                        (copy-file "completions/ya.bash"
-                                   (string-append bash "/ya"))
-                        (copy-file "completions/yazi.bash"
-                                   (string-append bash "/yazi"))
-                        (copy-file "completions/_ya"
-                                   (string-append zsh "/_ya"))
-                        (copy-file "completions/_yazi"
-                                   (string-append zsh "/_yazi"))))))
-       #:patchelf-plan `(("ya" ("gcc:lib" "glibc"))
-                         ("yazi" ("gcc:lib" "glibc")))
-       #:install-plan `(("ya" "bin/")
-                        ("yazi" "bin/"))))
-    (native-inputs (list unzip))
-    (inputs `(("gcc:lib" ,gcc "lib")
-              ("glibc" ,glibc)))
-    (supported-systems '("x86_64-linux"))
-    (synopsis
-     "Blazing fast terminal file manager written in Rust, based on async I/O")
-    (description
-     "Yazi is a terminal file manager written in Rust, based on non-blocking async I/O. It aims to provide an efficient, user-friendly, and customizable file management experience.")
-    (home-page "https://yazi-rs.github.io/")
+                      (let ((bin (string-append (assoc-ref outputs "out") "/bin")))
+                        (install-file "yazi" bin)
+                        (install-file "ya" bin)))))))
+    (home-page "https://yazi-rs.github.io")
+    (synopsis "Blazing fast terminal file manager written in Rust")
+    (description "Yazi is a terminal file manager written in Rust,
+based on async I/O. It aims to provide an efficient, user-friendly, and
+customizable experience.")
     (license license:expat)))
